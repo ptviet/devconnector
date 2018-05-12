71887e556add7412b3ec2d16f56d4c343e6c5cb3
ptviet <ptviet@live.com>
2018-05-12 23:02:21 +1000

    refactor code

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
-router.get("/test", (req, res) => res.json({msg: "Posts works"}));
+router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
 
 module.exports = router;
