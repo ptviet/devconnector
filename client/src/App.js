fb1474dff302353e876585818c3351dd025d9127
ptviet <ptviet@live.com>
2018-05-15 22:35:32 +1000

    modify App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
 import jwt_decode from "jwt-decode";
 import { Provider } from "react-redux";
 import store from "./store";
+import setAuthToken from "./utils/setAuthToken";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -13,6 +12,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import { clearCurrentProfile } from "./actions/profileActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 // Check for token
 if (localStorage.jwtToken) {
