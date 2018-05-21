b739cfbd76023a35f225f0151e55074053457e0c
ptviet <ptviet@live.com>
2018-05-21 21:57:13 +1000

    deploy

--- server.js
+++ server.js
@@ -34,16 +34,16 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 // if (process.env.NODE_ENV === "production") {
+//   // Set static folder
 //   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
 // }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`The server has started on port ${port}`));
